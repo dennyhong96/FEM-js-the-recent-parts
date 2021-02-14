31766d8d87fb515cf7b1dc7900b1282c3c0c3cee
Denny Hong <denny@beacondigitalmarketing.com>
2021-02-13 20:16:08 -0800

    Add destructuring solution

--- destructuring/ex.js
+++ destructuring/ex.js
@@ -3,43 +3,48 @@ var defaults = {
 	format: "Live",
 	slides: {
 		start: 0,
-		end: 100
-	}
+		end: 100,
+	},
 };
 
-fakeAjax("http://get-the-workshop.tld",handleResponse);
-
+fakeAjax("http://get-the-workshop.tld", handleResponse);
 
 // *******************************************************
 
-
-function handleResponse(/* destructuring here */) {
-
+function handleResponse({
+	/* destructuring here */
+	topic = "JavsScript",
+	format = "Live",
+	slides: { start = 0, end = 100 } = {},
+}) {
 	TestCase({
 		/* restructuring here */
+		topic,
+		format,
+		slides: {
+			start,
+			end,
+		},
 	});
-
 }
 
 function TestCase(data) {
 	console.log(
 		data.topic == "JS Recent Parts" &&
-		data.format == "Live" &&
-		data.slides.start === 0 &&
-		data.slides.end == 77
+			data.format == "Live" &&
+			data.slides.start === 0 &&
+			data.slides.end == 77
 	);
 }
 
-
 // *******************************************************
 
-
-function fakeAjax(url,cb) {
+function fakeAjax(url, cb) {
 	// fake ajax response:
 	cb({
 		topic: "JS Recent Parts",
 		slides: {
-			end: 77
-		}
+			end: 77,
+		},
 	});
 }
